c4c03243b8dbf3375ba588b5703cf41a9327778f
Scott Harvey <scottandrewharvey@gmail.com>
2013-03-13 17:29:05 +0100

    Update the classic layout to include a jump menu

--- resources/classic/docco.js
+++ resources/classic/docco.js
@@ -1,37 +1,36 @@
-var jSLink_els = document.getElementsByClassName("source")
-  , jSources = []
-  , jF_el = document.getElementById("jump_filter")
-  , jFC_el = document.getElementById("jump_filter_clear")
-  , jS_el = document.getElementById("jump_sources")
-  , jR_el = document.getElementById("jump_results");
+var sourceElements = document.getElementsByClassName("source"),
+  sourceNames = [],
+  filterInput = document.getElementById("jump_filter"),
+  sourcesWrapper = document.getElementById("jump_sources"),
+  resultsWrapper = document.getElementById("jump_results");
 
-function makefuzzy( txt ) {
+for (var i=0, j=sourceElements.length; i<j; i++) {
+  sourceNames.push(sourceElements[i].textContent.trim().toLowerCase());
+}
+
+function makefuzzy(txt) {
   return new RegExp(txt.split("").map(function( c ) {
     return "\\/?!,.:=-+*^$[]()".indexOf(c) > -1 ? "\\\\" + c : c;
   }).join(".*"));
 }
 
-function toggleJump( txt ) {
-  jS_el.style.display = txt ? "none" : "block";
-  jR_el.style.display = txt ? "block" : "none";
+function toggleJump(txt) {
+  sourcesWrapper.style.display = txt ? "none" : "block";
+  resultsWrapper.style.display = txt ? "block" : "none";
   return txt;
 }
 
-for (var i=0, j=jSLink_els.length; i<j; i++) {
-  jSources.push(jSLink_els[i].textContent.trim().toLowerCase());
-}
+filterInput.addEventListener("keyup", function() {
+  var txt = this.value.trim().toLowerCase(),
+    fuzz = makefuzzy(txt),
+    srcWeights = [];
 
-jF_el.addEventListener("keyup", function() {
-  var txt = this.value.trim().toLowerCase()
-    , fuzz = makefuzzy(txt)
-    , srcWeights = [];
-  
   if (!toggleJump(txt)) return true;
-  
-  for (var i=0, j=jSources.length; i<j; i++) {
-    switch (jSources[i].indexOf(txt)) {
+
+  for (var i=0, j=sourceNames.length; i<j; i++) {
+    switch (sourceNames[i].indexOf(txt)) {
       case -1:
-        if (fuzz.test(jSources[i]))
+        if (fuzz.test(sourceNames[i]))
           srcWeights.push({ weight: 3, idx: i });
         break;
       case 0:
@@ -41,12 +40,12 @@ jF_el.addEventListener("keyup", function() {
         srcWeights.push({ weight: 2, idx: i });
     }
   }
-  
-  jR_el.innerHTML = "";
+
+  resultsWrapper.innerHTML = "";
   srcWeights.sort(function( a, b ) {
     return a.weight - b.weight;
   }).forEach(function( src ) {
-    jR_el.appendChild(jSLink_els[src.idx].cloneNode(true));
+    resultsWrapper.appendChild(sourceElements[src.idx].cloneNode(true));
   });
 });
-jFC_el.addEventListener("click", function() { toggleJump(jF_el.value = ""); });
+
